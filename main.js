e8a9e330c1d8a74262ffc0853212500b9ecefefc
zawho <zthooper2@gmail.com>
2023-01-28 12:17:52 +0800

    Create func for add book button

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const pagesInput = document.querySelector('.pages-input');
-const yesRead = document.querySelector('.yes-read');
-const notRead = document.querySelector('.not-read');
+// const yesRead = document.querySelector('.yes-read');
+// const notRead = document.querySelector('.not-read');
 const addBookButton = document.querySelector('.add-book');
 
 // Main book storage array.
 let theLibrary = [];
 
 // Book constructor.
-function Book(title, author, pages, readStatus) {
+function Book(title, author, pages /* , readStatus */) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readStatus = readStatus;
+    // this.readStatus = readStatus;
 }
 
 // User book variable;
-let userBook = new Book();
+let userBook = new Book(titleInput.value, authorInput.value, pagesInput.value);
 
 // Add book to library.
 function addBook() {
     theLibrary.push(userBook);
+    console.log(theLibrary);
 }
 
-console.log(theLibrary);
+addBookButton.addEventListener('click', addBook);
