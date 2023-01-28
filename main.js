c36682ef864842192f9a30d928c9d44d8c1618fe
zawho <zthooper2@gmail.com>
2023-01-28 12:05:55 +0800

    Clean up JS code

--- main.js
+++ main.js
@@ -16,17 +16,12 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
-// Example book instance.
-Book.prototype.info = function getBookInfo() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
-}
-
-const nightwood = new Book('Nightwood', 'Djuna Barnes', '180', 'Have read');
+// User book variable;
+let userBook = new Book();
 
 // Add book to library.
 function addBook() {
-    theLibrary.push(Book);
+    theLibrary.push(userBook);
 }
 
-addBook();
 console.log(theLibrary);
