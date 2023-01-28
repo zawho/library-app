e5ff45cc4aa773f36dfdd60719e53e093945e984
zawho <zthooper2@gmail.com>
2023-01-28 10:56:55 +0800

    Add JS comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// Book constructor.
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
@@ -5,9 +7,10 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
+
+/* Example book instance.
 Book.prototype.info = function getBookInfo() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
 }
 const nightwood = new Book('Nightwood', 'Djuna Barnes', '180', 'Have read');
-
-console.log(nightwood.info());
+console.log(nightwood.info()); */
