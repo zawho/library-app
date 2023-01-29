5b5c3b28b06439f64517606196df0339afa9de24
zawho <zthooper2@gmail.com>
2023-01-29 09:36:03 +0800

    Add basic CSS to book input

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
 const pagesInput = document.querySelector('.pages-input');
 const yesRead = document.querySelector('.yes-read');
-const notRead = document.querySelector('.not-read');
 const addBookButton = document.querySelector('.add-book');
 
 // Main book storage array.
@@ -23,9 +22,9 @@ function addBook() {
     userBook.author = authorInput.value;
     userBook.pages = pagesInput.value;
     if (yesRead.checked) {
-        userBook.readStatus = 'Read';
-    } else if (notRead.checked) {
-        userBook.readStatus = 'Not read';
+        userBook.readStatus = 'Finished';
+    } else {
+        userBook.readStatus = 'Unfinished';
     }
     theLibrary.push(userBook);
     console.log(theLibrary);
