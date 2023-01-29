346a1489fe803a51cc731bc5158cf386d885f739
zawho <zthooper2@gmail.com>
2023-01-29 10:32:32 +0800

    Add HTML elements to display func

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const authorInput = document.querySelector('.author-input');
 const pagesInput = document.querySelector('.pages-input');
 const yesRead = document.querySelector('.yes-read');
 const addBookButton = document.querySelector('.add-book');
+const userLibrary = document.querySelector('.user-library');
 
 // Main book storage array.
 const theLibrary = [];
@@ -13,6 +14,26 @@ function Book() {
     this.author = '';
     this.pages = '';
     this.readStatus = '';
+}
+
+// Loop through array and display books.
+function displayBook() {
+    const bookCard = document.createElement('div');
+    const cardTitle = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardPages = document.createElement('div');
+    const cardReadStatus = document.createElement('div');
+    bookCard.classList.add('book-card');
+    for (let i = 0; i < theLibrary.length; i++) {
+            cardTitle.innerText = theLibrary[theLibrary.length - 1].title;
+            cardAuthor.innerText = theLibrary[theLibrary.length - 1].author;
+            cardPages.innerText = theLibrary[theLibrary.length - 1].pages;
+            userLibrary.appendChild(bookCard);
+            bookCard.appendChild(cardTitle);
+            bookCard.appendChild(cardAuthor);
+            bookCard.appendChild(cardPages);
+            bookCard.appendChild(cardReadStatus);
+    }
 }
 
 // Add book to library.
@@ -28,6 +49,7 @@ function addBook() {
     }
     theLibrary.push(userBook);
     console.log(theLibrary);
+    displayBook();
 }
 
 addBookButton.addEventListener('click', addBook);
